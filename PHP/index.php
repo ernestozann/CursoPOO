754dd63ec1427d2e0d99594539f21a9a72c837cb
Ernesto Martinez <ernestomm99@hotmail.com>
2021-05-31 12:12:09 -0600

    Solución del reto de herencia en PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once('Car.php');
-require_once('Account.php');
+require_once("Car.php");
+require_once("uberX.php");
+require_once("uberPool.php")
+require_once("Account.php");
 
-$car = new Car("AW456", new Account("Andres Herrera","AMS123"));
-$car->printDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dogde", "Attitude")
+$uberPool->printDataCar();
 ?>
